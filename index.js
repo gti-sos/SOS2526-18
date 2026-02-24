f8dbc5a1d092dd12f1e60d6f3c261fbc7af2ddfe
Kasthro <leunamanabc@gmail.com>
2026-02-24 18:15:03 +0100

    desarrollo api MCS

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express"; //Cargo la librería Express (el motor del servidor)
+import bodyParser from "body-parser";
 import cool from "cool-ascii-faces"; // Cargo la librería de las caritas
 import { calculateAverage, average, field, country} from "./index-JLAV.js";
 import { avgperCountry,datamcs} from "./index-MCS.js";
@@ -11,6 +12,9 @@ app.use(express.json()); //Para que el servidor entienda datos en formato JSON
 // Si la ruta solicitada no coincide con ninguna ruta dinámica (API), 
 // el servidor intentará localizar el archivo dentro de la carpeta './public'.
 app.use("/", express.static("./public")); 
+
+//activar body-parser para deserializar
+app.use(bodyParser.json());
 
 //Ruta para la carita ASCII
 app.get("/cool", (req, res) => {
@@ -30,8 +34,20 @@ let avg=avgperCountry(datamcs,"China, mainland","production_tonnes");
 app.get("/samples/mcs", (req, res) => {
     res.send("Average production in tonnes of mainland China: "+avg);
 });
+
 app.get("/api/v1/cereal-production", (req, res) => {
     res.send(`La media de cereal_production para Paraguay es: ${average}`);
+});
+
+//api MCS
+app.get("/api/v1/food-supply-utilization-accounts", (req, res) => {
+    res.send(JSON.stringify(datamcs,null,2));
+});
+
+app.post("/api/v1/food-supply-utilization-accounts", (req, res) => {
+    let newData=req.body;
+    datamcs.push(newData);
+    res.sendStatus(201,"CREATED")
 });
 
 //Encendemos el servidor
