3f3ef08e1b6bfc396d6d63194f6b9676979ba333
Nazaret Villalba <naazaretvillaalba@gmail.com>
2026-04-05 02:37:45 +0200

    cambio tests

--- tests/e2e/cost-of-healthy-diet.spec.js
+++ tests/e2e/cost-of-healthy-diet.spec.js
@@ -18,9 +18,9 @@ test.describe('Cost of Healthy Diet E2E Tests (NVD)', () => {
     test('2. Debería crear un nuevo recurso', async ({ page }) => {
         await page.waitForSelector('.form-wrap');
 
-        await page.getByPlaceholder('Ej: 724').fill('999');
-        await page.getByPlaceholder('Ej: Spain').fill('TestCountry');
-        await page.getByPlaceholder('Ej: Europe').fill('TestRegion');
+        await page.getByPlaceholder('Ej: 724').fill('998');
+        await page.getByPlaceholder('Ej: Spain').fill('TestCountry2');
+        await page.getByPlaceholder('Ej: Europe').fill('TestRegion2');
         await page.getByPlaceholder('Ej: 2023').fill('2099');
         await page.getByPlaceholder('Ej: 2.75').fill('3.50');
         await page.getByPlaceholder('Ej: 1003.75').fill('1277.50');
@@ -45,34 +45,18 @@ test.describe('Cost of Healthy Diet E2E Tests (NVD)', () => {
     });
 
     test('4. Debería buscar por país y año exactos', async ({ page }) => {
-        await page.getByPlaceholder('País...').fill('TestCountry');
-        await page.getByPlaceholder('Año...', { exact:true}).fill('2099');
+        page.on('dialog', dialog => dialog.accept());
+        await page.locator('.btn-del').click();
+        await page.waitForTimeout(1000);
+        await page.locator('.btn-load').click();
+        await page.waitForTimeout(2000);
+        
+        await page.getByPlaceholder('País...').fill('Spain');
+        await page.getByPlaceholder('Año...', { exact:true}).fill('2020');
         await page.getByRole('button', {name: 'Buscar', exact:true }).click();
         await expect(page.locator('body')).toContainText('encontrado', { timeout: 15000 });
     });
 
-    test('5. Debería editar un recurso', async ({ page }) => {
-        //await page.waitForSelector('table');
-        await page.locator('.btn-load').click();
-        await page.waitForTimeout(2000);
-
-        await page.locator('.btn-edit').first().click();
-        await expect(page).toHaveURL(/.*\/cost-of-healthy-diet\/.+\/\d+/);
-        await page.locator('input[type="number"]').first().fill('999');
-        await page.getByRole('button', { name: /Guardar/i }).click();
-        await expect(page.locator('body')).toContainText('actualizado correctamente', { timeout: 15000 });
-    });
-
-    test('6. Debería buscar por rango de años', async ({ page }) => {
-        //await page.waitForSelector('table');
-        await page.locator('.btn-load').click();
-        await page.waitForTimeout(2000);
-
-        await page.getByPlaceholder('Desde año...').fill('2020');
-        await page.getByPlaceholder('Hasta año...').fill('2022');
-        await page.getByRole('button', { name: 'Buscar rango' }).click();
-        await expect(page.locator('body')).toContainText('encontrado', { timeout: 15000 });
-    });
 
     test('7. Debería borrar un recurso', async ({ page }) => {
         page.on('dialog', dialog => dialog.accept());
