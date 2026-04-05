025b239d6c32f50cca0a19b7d1f1903616487d18
Nazaret Villalba <naazaretvillaalba@gmail.com>
2026-04-05 02:59:46 +0200

    cambios tests e2e

--- tests/e2e/cost-of-healthy-diet.spec.js
+++ tests/e2e/cost-of-healthy-diet.spec.js
@@ -57,6 +57,29 @@ test.describe('Cost of Healthy Diet E2E Tests (NVD)', () => {
         await expect(page.locator('body')).toContainText('encontrado', { timeout: 15000 });
     });
 
+        test('5. Debería editar un recurso', async ({ page }) => {
+        //await page.waitForSelector('table');
+        await page.locator('.btn-load').click();
+        await page.waitForTimeout(2000);
+
+        await page.locator('.btn-edit').first().click();
+        await expect(page).toHaveURL(/.*\/cost-of-healthy-diet\/.+\/\d+/);
+        await page.locator('input[type="number"]').first().fill('999');
+        await page.getByRole('button', { name: /Guardar/i }).click();
+        await expect(page.locator('body')).toContainText('actualizado correctamente', { timeout: 15000 });
+    });
+
+    test('6. Debería buscar por rango de años', async ({ page }) => {
+        //await page.waitForSelector('table');
+        await page.locator('.btn-load').click();
+        await page.waitForTimeout(2000);
+
+        await page.getByPlaceholder('Desde año...').fill('2020');
+        await page.getByPlaceholder('Hasta año...').fill('2022');
+        await page.getByRole('button', { name: 'Buscar rango' }).click();
+        await expect(page.locator('body')).toContainText('encontrado', { timeout: 15000 });
+    });
+
     test('7. Debería borrar un recurso', async ({ page }) => {
         page.on('dialog', dialog => dialog.accept());
         await page.locator('.btn-delete').first().click();
