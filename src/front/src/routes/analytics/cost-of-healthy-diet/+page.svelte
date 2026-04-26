0183357c355ff564b34be9b9e4a87ce3fdcaae07
Nazaret Villalba <naazaretvillaalba@gmail.com>
2026-04-26 20:56:47 +0200

    .

--- src/front/src/routes/analytics/cost-of-healthy-diet/+page.svelte
+++ src/front/src/routes/analytics/cost-of-healthy-diet/+page.svelte
@@ -49,6 +49,7 @@
         const avgVeg    = countries.map(c => parseFloat((countryMap[c].veg   / countryMap[c].count).toFixed(2)));
         const avgFruit  = countries.map(c => parseFloat((countryMap[c].fruit / countryMap[c].count).toFixed(2)));
 
+
         Highcharts.chart('container-diet', {
             chart: {
                 type: 'bar',
